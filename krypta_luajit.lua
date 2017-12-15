7e525556bdf3b145d0b15a818ef0248e626b6659
Frantisek Fuka <fuka@fuxoft.cz>
2017-12-15 15:33:01 +0100

    Added link to github.

--- krypta_luajit.lua
+++ krypta_luajit.lua
@@ -1,5 +1,6 @@
 #!/usr/bin/env luajit
---version 20171215
+-- https://github.com/fuxoft/krypta
+-- version 20171215c
 
 --[[
 	Set the SALT to something you can easily remember.
@@ -1461,7 +1462,7 @@ local function main()
 			local ichksum = sha256(strseed0..zeroes..dwords_to_chars(result)..zeroes.."index checksum", "dwords")
 			local chw1, chw2 = band(ichksum[1], 0x7ff), band(ichksum[2], 0x7ff)
 			if prefix then
-				print(string.format("Checkwords for this specific master passphrase, index and prefix: '%s'", checkwords(strseed0..dwords_to_chars(result)..prefix, 3)))
+				print(string.format("Checkwords for this specific master passphrase, index and prefix (%s:): '%s'", prefix, checkwords(strseed0..dwords_to_chars(result)..prefix, 3)))
 			else
 				print(string.format("Checkwords for this specific master passphrase and index: '%s'", checkwords(strseed0..dwords_to_chars(result))))
 			end
